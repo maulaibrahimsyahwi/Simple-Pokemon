0785af64ed2a1193e97a68d08dcaa52bd13cf0ff
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-08-11 10:19:25 +0700

    first

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/simple-pokemon",
+  base: "/simple-pokemon/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash][extname]",
+      },
+    },
+  },
 });
