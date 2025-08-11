bdbd1e5df59893ccce54ecff26471035f7296896
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-08-11 10:54:49 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/simple-pokemon/",
+  base: process.env.VITE_BASE_PATH || "/",
   build: {
     rollupOptions: {
       output: {
